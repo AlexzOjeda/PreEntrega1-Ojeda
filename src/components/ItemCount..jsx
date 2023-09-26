d172a86cb7905b1390d1badc1205728342a5c396
Alejandro Ojeda <111455664+AlexzOjeda@users.noreply.github.com>
2023-09-25 21:26:16 -0300

    agregando informacion sobre avance segunda parte

--- src/components/ItemCount..jsx
+++ src/components/ItemCount..jsx
@@ -3,14 +3,14 @@ import useState from 'react'
 import useEffect from 'react'
 
 const ItemCount = ({initial, stock, onAdd}) => {
-    const [count, setCount] = useState(parseInt(initial));
+    const [count, setCount] = useState(parseInt(initial))
    
     const increase = () => {
-        setCount(count + 1);
+        setCount(count + 1)
     };
 
     const decrease = () => {
-        setCount(count - 1);
+        setCount(count - 1)
     };
 
     useEffect(() => {
@@ -19,12 +19,11 @@ const ItemCount = ({initial, stock, onAdd}) => {
 
     return (
         <div> <h2>Contador</h2>
-            <button disabled={count <= 1} onClick={increase}>Suma</button>
+            <button disabled={count <= 1} onClick={decrease}>resta</button>
             <span>{count}</span>
-            <button disabled={count >= stock} onClick={increase}>Suma</button>
+            <button disabled={count >= stock} onClick={increase}>suma</button>
             <div> 
-                <button disabled={stock <=0} onClick={() => onAdd(count)}> Agregar al carro 
-                </button>
+                <button disabled={stock <=0} onClick={() => onAdd(count)}> Agregar al carro </button>
                 </div>
         </div>
     );
